5a8a0280c149670e2ac9b72e2849a95fce1888f4
kongshan <kongshan@dtstack.com>
2021-02-20 15:29:11 +0800

    feat: add strategy

--- src/views/backStage/strategy/index.js
+++ src/views/backStage/strategy/index.js
@@ -1,9 +1,48 @@
+import { Input, Tag, Button } from 'antd'
 import './index.scss'
 
+const { Search } = Input;
+
 function Strategy(props) {
+
   return (
-    <div>
-      Strategy
+    <div className="backStage-strategy-main">
+      <div className="strategy-header">
+        <Search
+          className="dt-search"
+          placeholder="请输入关键词"
+          allowClear
+        />
+        <Input allowClear className="add-input"/>
+        <Button type="primary">增加</Button>
+      </div>
+      <div className="tags-div">
+        <Tag color="magenta" closable>magenta</Tag>
+        <Tag color="red" closable>red</Tag>
+        <Tag color="volcano" closable>volcano</Tag>
+        <Tag color="orange" closable>orange</Tag>
+        <Tag color="gold" closable>gold</Tag>
+        <Tag color="lime" closable>lime</Tag>
+        <Tag color="green" closable>green</Tag>
+        <Tag color="cyan" closable>cyan</Tag>
+        <Tag color="blue" closable>blue</Tag>
+        <Tag color="geekblue" closable>geekblue</Tag>
+        <Tag color="purple" closable>purple</Tag>
+        <Tag color="magenta" closable>magenta</Tag>
+        <Tag color="red" closable>red</Tag>
+        <Tag color="volcano" closable>volcano</Tag>
+        <Tag color="orange" closable>orange</Tag>
+        <Tag color="gold" closable>gold</Tag>
+        <Tag color="lime" closable>lime</Tag>
+        <Tag color="green" closable>green</Tag>
+        <Tag color="cyan" closable>cyan</Tag>
+        <Tag color="blue" closable>blue</Tag>
+        <Tag color="geekblue" closable>geekblue</Tag>
+        <Tag color="purple" closable>purple</Tag>
+      </div>
+      <div>
+        <h2>示例</h2>
+      </div>
     </div>
   )
 }
