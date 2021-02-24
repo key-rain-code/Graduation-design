0c51b79971091e41f083f575fc3c194430848a50
kongshan <kongshan@dtstack.com>
2021-02-24 14:34:27 +0800

    feat: use yarn2 and pnp

--- src/views/backStage/strategy/index.js
+++ src/views/backStage/strategy/index.js
@@ -12,11 +12,13 @@ function Strategy(props) {
           className="dt-search"
           placeholder="请输入关键词"
           allowClear
+          enterButton
+          bordered={false}
+          style={{boxShadow:'0px 2px 8px 0px rgb(6 14 26 / 8%)', backgroundColor: '#FFFFFF', borderRadius: 2}}
         />
-        <Input allowClear className="add-input"/>
         <Button type="primary">增加</Button>
       </div>
-      <div className="tags-div">
+      <div className="tags-div" style={{boxShadow:'0px 2px 8px 0px rgb(6 14 26 / 8%)', backgroundColor: '#FFFFFF', borderRadius: 2}}>
         <Tag color="magenta" closable>magenta</Tag>
         <Tag color="red" closable>red</Tag>
         <Tag color="volcano" closable>volcano</Tag>
